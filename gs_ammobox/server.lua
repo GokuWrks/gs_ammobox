ca1765af62e78b34069d564d5e886be9d9305f84
GS Goku <142231319+GokuWrks@users.noreply.github.com>
2023-11-04 18:29:16 +0100

    Update server.lua

--- gs_ammobox/server.lua
+++ gs_ammobox/server.lua
@@ -17,31 +17,31 @@ end
 
 -- Items 
 ESX.RegisterUsableItem('ammo-9boxs', function(source)
-    addAmmo(source, 'ammo-9boxs', 'ammo-9', 30, 'Du hast 30x 9mm Patronen erhalten.')
+    addAmmo(source, 'ammo-9boxs', 'ammo-9', 30, 'Du hast ~g~30x~s~ 9mm Patronen erhalten.')
 end)
 
 ESX.RegisterUsableItem('ammo-9boxl', function(source)
-    addAmmo(source, 'ammo-9boxl', 'ammo-9', 90, 'Du hast 90x 9mm Patronen erhalten.')
+    addAmmo(source, 'ammo-9boxl', 'ammo-9', 90, 'Du hast ~g~90x~s~ 9mm Patronen erhalten.')
 end)
 
 ESX.RegisterUsableItem('ammo-45boxs', function(source)
-    addAmmo(source, 'ammo-45boxs', 'ammo-45', 20, 'Du hast 20x .45 ACP Patronen erhalten.')
+    addAmmo(source, 'ammo-45boxs', 'ammo-45', 20, 'Du hast ~g~20x~s~ .45 ACP Patronen erhalten.')
 end)
 
 ESX.RegisterUsableItem('ammo-45boxl', function(source)
-    addAmmo(source, 'ammo-45boxl', 'ammo-45', 60, 'Du hast 60x .45 ACP Patronen erhalten.')
+    addAmmo(source, 'ammo-45boxl', 'ammo-45', 60, 'Du hast ~g~60x~s~ .45 ACP Patronen erhalten.')
 end)
 
 ESX.RegisterUsableItem('ammo-riflebox', function(source)
-    addAmmo(source, 'ammo-riflebox', 'ammo-rifle', 90, 'Du hast 90x 5.56x45 Patronen erhalten.')
+    addAmmo(source, 'ammo-riflebox', 'ammo-rifle', 90, 'Du hast ~g~90x~s~ 5.56x45 Patronen erhalten.')
 end)
 
 ESX.RegisterUsableItem('ammo-sniperbox', function(source)
-    addAmmo(source, 'ammo-sniperbox', 'ammo-sniper', 12, 'Du hast 12x 7.62x51 Patronen erhalten.')
+    addAmmo(source, 'ammo-sniperbox', 'ammo-sniper', 12, 'Du hast ~g~12x~s~ 7.62x51 Patronen erhalten.')
 end)
 
 -- Example 
 
 -- ESX.RegisterUsableItem('example-item1', function(source)
---     addAmmo(source, 'example-item1', 'example-item', 1, 'Du hast 1x example-item erhalten.')
+--     addAmmo(source, 'example-item1', 'example-item', 1, 'Du hast ~g~1x example-item erhalten.')
 -- end)
